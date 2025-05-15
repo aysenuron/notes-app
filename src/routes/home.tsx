4dbf1ffc9d57e6967a9d43301fc8b2766021f9bb
aysenuron <onaranays@gmail.com>
2025-05-15 11:45:18 +0300

    Change filtering method from screen only js to searchparams.

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router"
+import { Link, useSearchParams, useNavigate } from "react-router"
 import { Button } from "@/components/ui/button"
 import CardItem from "@/components/ui/CardItem"
 import { Badge } from "@/components/ui/badge"
@@ -10,9 +10,14 @@ import { Tag, Note } from "@/utils/types"
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(false);
   const [notes, setNotes] = useState<Note[]>([]);
-  const [inputText, setInputText] = useState<string>("");
   const [tags, setTags] = useState<Tag[]>([]);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const tagsFilter = searchParams.get("tags")?.split(",") ?? [];
+  const searchFilter = searchParams.get("search")?.toLowerCase() ?? "";
+  console.log(tagsFilter, searchFilter);
 
   useEffect(() => {
     async function loadNotes() {
@@ -34,20 +39,31 @@ export default function Home() {
 
   function inputHandler(e: React.ChangeEvent<HTMLInputElement>) {
     const searchWord = e.target.value.toLowerCase();
-    setInputText(searchWord);
+    updateURLParams("search", searchWord);
   }
 
   function tagHandler(tag: Tag) {
-    setSelectedTags(prevTags => (
-      prevTags.includes(tag) ? prevTags.filter(t => t !== tag) : [...prevTags, tag]));
+    let newTags: string[];
+    tagsFilter.includes(tag) ? newTags = tagsFilter.filter(t => t !== tag): newTags = [...tagsFilter, tag];
+    updateURLParams("tags", newTags);
   }
 
   const filteredNotes = notes.filter((note: Note) => {
-    const lowerCase = inputText.toLowerCase();
-    const matchesInput = note.content.toLowerCase().includes(lowerCase) || note.title.toLowerCase().includes(lowerCase);
-    const matchesTag = selectedTags.length === 0 || note.tags.some((tag: Tag) => selectedTags.includes(tag));
+    const matchesInput = note.content.toLowerCase().includes(searchFilter) || note.title.toLowerCase().includes(searchFilter);
+    const matchesTag = tagsFilter.length === 0 || note.tags.some((tag: Tag) => tagsFilter.includes(tag));
     return matchesInput && matchesTag;
   });
+
+  function updateURLParams(key: string, value: string | string[] | null) {
+    const params = new URLSearchParams(searchParams);
+  
+    if (value && (Array.isArray(value) ? value.length > 0 : value !== "")) {
+      params.set(key, Array.isArray(value) ? value.join(",") : value);
+    } else {
+      params.delete(key);
+    }
+    navigate(`?${params.toString()}`);
+  }
 
   const noteElements = filteredNotes.map(note => (
     <Link to={`/${note.id}`}
@@ -65,7 +81,7 @@ export default function Home() {
     return (
         <>
         <div><Input onChange={inputHandler} className="w-full lg:w-1/2 xl:w-1/3" type="text" placeholder="Search" /></div>
-        <div className="flex gap-2 mt-5 flex-wrap">{tags.map(tag => <Badge key={tag} variant={selectedTags.includes(tag) ? "default" : "secondary"} className="cursor-pointer" onClick={() => tagHandler(tag)}>{tag}</Badge>)}</div>
+        <div className="flex gap-2 mt-5 flex-wrap">{tags.map(tag => <Badge key={tag} variant={tagsFilter.includes(tag) ? "default" : "secondary"} className="cursor-pointer" onClick={() => tagHandler(tag)}>{tag}</Badge>)}</div>
         <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 mt-5">
           {loading ? <h2>Notes are loading...</h2> : noteElements}
         </main>
