aaf4af6e4a2ad4e8a1ad99b88bb0fffb3e51a345
aysenuron <onaranays@gmail.com>
2025-05-17 17:05:34 +0300

    Add remove tag functionality.

--- src/routes/newNote.tsx
+++ src/routes/newNote.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button"
 import { Trash } from 'lucide-react';
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { X } from 'lucide-react';
 
 export default function NewNote() {
     const [textTag, setTextTag] = useState<string>("");
@@ -40,8 +41,8 @@ export default function NewNote() {
 
       const filteredTagsElement = filteredTags.map(tag => (
         <div key={tag}>
-            <Button className="m-2" variant={"ghost"} onClick={() => selectTags(tag)}>
-                <p>{tag}</p>
+            <Button className="m-2 text-foreground" variant={"ghost"} onClick={() => selectTags(tag)}>
+                {tag}
             </Button>
             {tag === filteredTags[filteredTags.length - 1] ? null : <Separator className="my-2" />}
         </div>
@@ -62,7 +63,13 @@ export default function NewNote() {
         setTextTag("");
       }
 
-      const selectedTagElements = selectedTags.map(tag => <Badge key={tag}>{tag}</Badge>);
+      const removeTag = (tag: Tag) => {
+        if(selectedTags.includes(tag)) {
+            setSelectedTags(prevTags => prevTags.filter(t => t !== tag))
+        }
+      };
+
+      const selectedTagElements = selectedTags.map(tag => <Badge key={tag}>{tag} <Button onClick={() => removeTag(tag)} variant={"secondary"} className="ml-0.5 h-1 w-1 rounded-full"><X /></Button></Badge>);
 
     return (
         <>
@@ -70,7 +77,7 @@ export default function NewNote() {
                 <GoBackButton />
                 <div className="flex gap-5 items-center">
                 <Button variant={"ghost"}>
-                    <Trash className="h-4" />
+                    <Trash className="h-4 text-foreground" />
                 </Button>
                 <Button>Save</Button>
                 </div>
@@ -79,24 +86,30 @@ export default function NewNote() {
                 <form action="">
                     <Input type="text" placeholder="Title" className="w-full md:w-1/3" />
                     <div className="mt-8">
-                        <Textarea placeholder="Type your note here." />
+                        <Textarea placeholder="Type your note here." className="text-foreground" />
                     </div>
                     <div className="mt-8 bg-secondary p-6 rounded-2xl w-full lg:w-1/2">
-                        <div className="flex w-full items-center space-x-2">
-                            <div className="relative w-full">
-                                <Input className="bg-background" type="text" value={textTag} onChange={handleTagInputChange} placeholder="Add tag..." />
-                                {textTag 
-                                ? 
-                                <div className="absolute mt-4">
-                                <ScrollArea className="w-48 bg-background rounded-md border border-secondary-foreground">
-                                    {filteredTagsElement}
-                                </ScrollArea>
+                            <div className="flex w-full items-center space-x-2">
+
+                                <div className="relative w-full">
+                                    <Input className="bg-background" 
+                                    type="text" value={textTag} 
+                                    onChange={handleTagInputChange} 
+                                    placeholder="Add tag..."
+                                    onKeyDown={(e) => { if (e.key === "Enter") { e.preventDefault(); handleNewTag(textTag); } }}
+                                    />
+                                    {(filteredTags.length > 0)
+                                    ? 
+                                    <div className="absolute mt-4">
+                                    <ScrollArea className="w-48 bg-background rounded-md border border-secondary-foreground">
+                                        {filteredTagsElement}
+                                    </ScrollArea>
+                                    </div>
+                                    : null
+                                    }
                                 </div>
-                                : null
-                                }
+                                <Button onClick={() => handleNewTag(textTag)} type="submit">Add</Button>
                             </div>
-                            <Button onClick={() => handleNewTag(textTag)} type="button">Add</Button>
-                        </div>
                         <div className="flex gap-2 mt-4">
                             {selectedTagElements}
                         </div>
